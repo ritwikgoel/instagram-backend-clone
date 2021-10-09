911fcc4ddb011c4a8f7b9a52b968ccf655f0d13b
Ritwik <ritwikgoel@yahoo.com>
2021-10-09 14:19:05 +0530

    fixing mongodriver

--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/ritwikgoel/instagram-backend-clone
 
 go 1.12
+
+require (
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.mongodb.org/mongo-driver v1.7.3
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- server/server.go
+++ server/server.go
@@ -1,11 +1,61 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type User struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email  string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password  string             `json:"password,omitempty" bson:"password,omitempty"`
+}
+var client *mongo.Client
+
+
+func homePage(response http.ResponseWriter, request *http.Request){
+	fmt.Fprint(response,"Home Page")
+}
+
+func createUser(response http.ResponseWriter, request *http.Request){
+	response.Header().Add("content-type","application/json")
+	var user User
+	json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("ritwikgoel").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+	
+}
+
+func GetUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var user User
+	collection := client.Database("ritwikgoel").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	collection.Find(ctx, bson.M{})
+	json.NewEncoder(response).Encode(user)
+}
+
+
 func main(){
-	fmt.Print("initialization")
-	http.ListenAndServe(":8081",nil)//nil is default handler
+	fmt.Print("Starting")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+
+	http.HandleFunc("/",homePage)
+	//createUserr:=createUser()
+	http.HandleFunc("/createUser",createUser)
+	http.HandleFunc("/GetUser",GetUser)
+	http.ListenAndServe(":8080",nil)
 }
