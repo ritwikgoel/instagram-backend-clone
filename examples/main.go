e8a4ba1b81d9fd090eff75e79c678684fa7e4cf0
Ritwik <ritwikgoel@yahoo.com>
2021-10-09 15:53:11 +0530

    Feat: Posts structure and get/post methods

--- examples/main.go
+++ examples/main.go
@@ -8,8 +8,6 @@ import (
 	"log"
 	"net/http"
 	"time"
-
-	//"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -21,13 +19,33 @@ type User struct {
 	Password  string        
 }
 
+type Posts struct {
+	USERID string
+	ID    string   
+	Caption string             
+	URL  string           
+	Time  string        
+}
+
 func users(response http.ResponseWriter, request *http.Request){
 	response.Header().Add("content-type","application/json")
 	data := User{"1", "ash", "example@gmail.com","admin"}
 	b, _ := json.Marshal(data)
-
-
+	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	collection := Clientt.Database("admin").Collection("admin")
+	collection.InsertOne(context.TODO(), data)
 	http.Post("localhost:8080/users", "application/json",bytes.NewBuffer(b))
+	
+}
+
+func posts(response http.ResponseWriter, request *http.Request){
+	response.Header().Add("content-type","application/json")
+	data := Posts{"1", "2", "golang is hard to learn","googleimages.com","2021-01-01 00:00:01"}
+	b, _ := json.Marshal(data)
+	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	collection := Clientt.Database("admin").Collection("posts")
+	collection.InsertOne(context.TODO(), data)
+	http.Post("localhost:8080/posts", "application/json",bytes.NewBuffer(b))
 	
 }
 func getuser(response http.ResponseWriter, request *http.Request){
@@ -35,6 +53,21 @@ func getuser(response http.ResponseWriter, request *http.Request){
 	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
 	collection := Clientt.Database("admin").Collection("admin")
 	fmt.Print(collection.FindOne(context.TODO(),request.URL.String()))	
+
+}
+
+func getposts(response http.ResponseWriter, request *http.Request){
+	response.Header().Add("content-type","application/json")
+	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	collection := Clientt.Database("admin").Collection("posts")
+	fmt.Print(collection.FindOne(context.TODO(),request.URL.String()))	
+
+}
+func listall(response http.ResponseWriter, request *http.Request){
+	response.Header().Add("content-type","application/json")
+	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	collection := Clientt.Database("admin").Collection("posts")
+	fmt.Print(collection)
 
 }
 
@@ -55,19 +88,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(databases)
-	
-	// data := User{"1", "ash", "example@gmail.com","admin"}
-	// collection.InsertOne(context.TODO(), data)
-	// if err != nil {
-    // 	log.Fatal(err)
-	// }
 	http.HandleFunc("/users",users)
+	http.HandleFunc("/posts",posts)
 	http.HandleFunc("/users/find/",getuser)
+	http.HandleFunc("/posts/find/",getposts)
+	http.HandleFunc("/listallposts/",listall)
 	http.ListenAndServe(":8080",nil)
 
 }
