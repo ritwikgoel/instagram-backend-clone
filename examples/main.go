22bfd845081ac2ea6fdf8c7cbf6bbe53ef147c47
Ritwik <ritwikgoel@yahoo.com>
2021-10-09 15:03:17 +0530

    Fix: working database

--- examples/main.go
+++ examples/main.go
@@ -1,35 +1,66 @@
 package main
 
 import (
+	 "bytes"
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
+    "encoding/json"
 
-	"go.mongodb.org/mongo-driver/bson"
+	//"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+type User struct {
+	ID    string   
+	Name string             
+	Email  string           
+	Password  string        
+}
+
+func users(response http.ResponseWriter, request *http.Request){
+	response.Header().Add("content-type","application/json")
+	data := User{"1", "ash", "example@gmail.com","admin"}
+	b, _ := json.Marshal(data)
+
+
+	http.Post("localhost:8080/users", "application/json",bytes.NewBuffer(b))
+	
+}
 
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	fmt.Print("init")
+	Clientt, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	//Yes i know i pushed the password. 
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
+	err = Clientt.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer client.Disconnect(ctx)
-	err = client.Ping(ctx, readpref.Primary())
+	defer Clientt.Disconnect(ctx)
+	err = Clientt.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
 	}
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	// databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(databases)
+	collection := Clientt.Database("admin").Collection("admin")
+	fmt.Print(collection)
+	data := User{"1", "ash", "example@gmail.com","admin"}
+	collection.InsertOne(context.TODO(), data)
 	if err != nil {
-		log.Fatal(err)
+    	log.Fatal(err)
 	}
-	fmt.Println(databases)
+	http.HandleFunc("/users",nil)
+	http.ListenAndServe(":8080",nil)
+
 }
