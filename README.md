f85c69737919e4efd840d94c30a7b63b73bf9805
Ritwik <ritwikgoel@yahoo.com>
2021-10-09 20:50:01 +0530

    Fix: Added SHA256 to passwords and improved Readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# instagram-backend-clone
+# Instagram Backend CLone in Golang
+
+Task for appointy internship. 
+
+- Golang✨
+- MongoDB✨
+
+## Run
+
+Install golang and all the basic dependencies. Then start the server
+```sh
+cd server
+go run server.go
+```
+
+For tests...
+
+```sh
+cd server 
+go test server_test.go
+```
+
+## Plugins
+
+No external libraries 
+
--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -27,9 +29,19 @@ type Posts struct {
 	Time  string        
 }
 
+//Hashing for the password. Using SHA256 for hashing
+func HashIt(b string) string{
+	rec:=b
+	hh:=sha256.New()
+	hh.Write([]byte(rec))
+	hashed:=hh.Sum(nil)
+	return hex.EncodeToString(hashed)
+
+}
+
 func users(response http.ResponseWriter, request *http.Request){
 	response.Header().Add("content-type","application/json")
-	data := User{"1", "ash", "example@gmail.com","admin"}
+	data := User{"1", "ash", "example@gmail.com",HashIt("admin")}
 	b, _ := json.Marshal(data)
 	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
 	collection := Clientt.Database("admin").Collection("admin")
@@ -48,51 +60,56 @@ func posts(response http.ResponseWriter, request *http.Request){
 	http.Post("localhost:8080/posts", "application/json",bytes.NewBuffer(b))
 	
 }
+
 func Getuser(response http.ResponseWriter, request *http.Request){
 	response.Header().Add("content-type","application/json")
 	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
 	collection := Clientt.Database("admin").Collection("admin")
 	fmt.Print(collection.FindOne(context.TODO(),request.URL.String()))	
-
 }
+
 
 func getposts(response http.ResponseWriter, request *http.Request){
 	response.Header().Add("content-type","application/json")
 	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
 	collection := Clientt.Database("admin").Collection("posts")
 	fmt.Print(collection.FindOne(context.TODO(),request.URL.String()))	
-
 }
+
+
 func listall(response http.ResponseWriter, request *http.Request){
 	response.Header().Add("content-type","application/json")
 	Clientt, _ := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
 	collection := Clientt.Database("admin").Collection("posts")
 	fmt.Print(collection)
-
 }
+
 
 func main() {
 	fmt.Print("init")
 	Clientt, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin@cluster0.9w0dh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
-	//Yes i know i pushed the password. 
+	//Pushed Password to github. ****** 
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	//Connect the client to the DB
 	err = Clientt.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+	//Can omit. At the end, the client will get disconnected. defer acts like a stack.LIFO
 	defer Clientt.Disconnect(ctx)
+	//Pinging the client. Making sure about connections
 	err = Clientt.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
 	}
+	//Handing routes
 	http.HandleFunc("/users",users)
 	http.HandleFunc("/posts",posts)
 	http.HandleFunc("/users/find/",Getuser)
 	http.HandleFunc("/posts/find/",getposts)
 	http.HandleFunc("/listallposts/",listall)
-	http.ListenAndServe(":8080",nil)
-
+	http.ListenAndServe(":8080",nil)//Listening on port 8080
 }
